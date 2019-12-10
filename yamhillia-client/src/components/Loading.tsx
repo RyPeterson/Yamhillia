83457a9ba2fbc1bdd1d37988032207e8f06dd46f
Ryan Peterson <r.peterson104@gmail.com>
2019-12-09 17:21:29 -0800

    Updated server to dotnet core 3.1

--- yamhillia-client/src/components/Loading.tsx
+++ yamhillia-client/src/components/Loading.tsx
@@ -1,21 +1,42 @@
-import React, { FC } from "react";
-import styled from "styled-components";
+import React, { FC, ReactNode } from "react";
+import styled, { keyframes } from "styled-components";
 import Column from "./Column";
 import { MoonLoader } from "react-spinners";
 
-const Loading: FC = props => (
-  <Root {...props}>
-    <MoonLoader />
+interface LoadingProps {
+  children?: ReactNode;
+}
+
+const DefaultMessage: FC = props => (
+  <>
     <div>Loading. </div>
     <div>Please Wait...</div>
+  </>
+);
+
+const Loading: FC<LoadingProps> = ({ children, ...rest }) => (
+  <Root {...rest}>
+    <MoonLoader />
+    {children || <DefaultMessage />}
   </Root>
 );
 
 export default styled(Loading)``;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 const Root = styled(Column)`
   height: 100%;
   width: 100%;
   justify-content: center;
   align-items: center;
+  opacity: 0;
+  animation: ${fadeIn} 0.5s forwards;
 `;
